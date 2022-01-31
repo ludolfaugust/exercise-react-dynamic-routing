10e69e51edc582082bb5e1d22c5d8bfde2a47182
ludo <85556848+ludolfaugust@users.noreply.github.com>
2022-01-31 14:31:24 +0100

    add styling

--- src/Components/Pages/NewFacts.js
+++ src/Components/Pages/NewFacts.js
@@ -1,19 +1,25 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-
+import "./NewFacts.css";
 function NewFacts({ catFacts }) {
   const { id } = useParams();
   console.log(catFacts);
 
   return (
     <div>
-      <h1>Newest CatFacts</h1>
+      <h1 className="cat-fact-container__header">Newest CatFacts</h1>
       {catFacts
         .filter((catFact) => catFact._id === id)
         .map((catFact) => {
           return (
             <div>
-              <h2>{catFact.text.toUpperCase()}</h2>
+              <h2 className="cat-fact-container__text">
+                {catFact.text.toUpperCase()}
+              </h2>
+              <p>
+                copyright by {catFact._id.toLowerCase()} created at{" "}
+                {catFact.createdAt}
+              </p>
             </div>
           );
         })}
